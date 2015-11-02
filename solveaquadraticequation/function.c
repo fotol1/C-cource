9ecf24b33db1baee73f00dd798718244501959d4
fotol1 <fotol@bk.ru>
2015-11-02 20:04:30 +0300

    Indents was corrected

--- solveaquadraticequation/function.c
+++ solveaquadraticequation/function.c
@@ -1,20 +1,21 @@
-int solution( double a, double b, double c)
+int solution(double a, double b, double c)
 {
-    if (a == 0)
+	if (a == 0)
 	{
-		if (!b * !c)
-			return 1;
+        	if (!b * !c)
+            		return 1;
 
-        if (b != 0 && c == 0)
-            return 3;
+        	if (b != 0 && c == 0)
+            		return 3;
 
-		if (b *  c)
-			return 4;
-        return 2;
+        	if (b *  c)
+            		return 4;
+       
+		return 2;
 	}
 
 	if (a * (!b * !c))
-			return 3;
+        	return 3;
 
 	return 6;
 }
